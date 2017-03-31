cf8d3b0343a657d2f2a5f2251e10a9c64ee82fca
Icaro Pires <icaropiresdf@hotmail.com>
2017-03-31 03:01:55 +0000

    Add requirements and fix app name
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- funbox/core/settings.py
+++ funbox/core/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'funbox.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'funbox.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
