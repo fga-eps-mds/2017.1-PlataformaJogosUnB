c7cd30867e9046b2857348e17d55657ec3bb2b4d
Victor Leite <victorsfleite@gmail.com>
2017-04-03 01:01:27 -0300

    Add 'forgot password' functionality routes.
    Signed-off-by: Victor Leite <victorsfleite@gmail.com>

--- funbox/project/urls.py
+++ funbox/project/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import (
+    password_reset, password_reset_done, password_reset_confirm,
+    password_reset_complete,
+)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^reset-password/$', password_reset, name='reset_password'),
+    url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset-password/complete/$', password_reset_complete, name='password_reset_complete'),
 ]
