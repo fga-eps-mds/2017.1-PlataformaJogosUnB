548f9b5e0cb77b5222a78a3e34e5b58eaf888a4a
Icaro Pires <icaropiresdf@hotmail.com>
2017-04-05 14:05:12 +0000

    Create model Game
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- funbox/game/models.py
+++ funbox/game/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+	name =  models.CharField(
+		max_length=100,
+		null=False,
+		blank=False,
+		help_text="Qual o nome do jogo?"
+	)
+	
+	def save(self, *args, **kwargs):
+		self.clean_fields()
+		super(Game, self).save(*args, **kwargs)
+
+	def __str__(self):
+		return self.name
