15d81add246d88853e693356cef556b5eca0fdfe
Icaro Pires <icaropiresdf@hotmail.com>
2017-04-05 16:08:48 +0000

    Create Information model and add game_version to Game model
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- funbox/game/models.py
+++ funbox/game/models.py
@@ -1,12 +1,33 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+import re
+
+def validate_version(version):
+	version_pattern = '^(\d+\.)*\d+$'
+	if re.match(version_pattern, version) is None:
+		raise ValidationError(
+			_("The version characters can only be or a  '.' or a digit and can't have 2 follows '.'. Error at: %(version)s"),
+			params={'version': version},
+		)
 
 class Game(models.Model):
 	name =  models.CharField(
+		_('Name'),
 		max_length=100,
 		null=False,
 		blank=False,
-		help_text="Qual o nome do jogo?"
-	)
+		help_text=_("What's the name of the game?")
+	)	
+
+	game_version = models.CharField(
+		_('Game version'),
+                max_length=20,
+		validators=[validate_version],
+                null=True,
+                blank=True,
+                help_text=_("What's the game version")
+        )
 	
 	def save(self, *args, **kwargs):
 		self.clean_fields()
@@ -14,3 +35,20 @@ class Game(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class Information(models.Model):
+        description = models.TextField(
+		_('Description'),
+                max_length=1000,
+                null=False,
+                blank=False,
+                help_text=_("Describe your game")
+        )
+
+        launch_year = models.IntegerField(
+		_('Launch year'),
+                null=False,
+                blank=False,
+                help_text=_("Which was the year the game was launched?")
+        )
+
