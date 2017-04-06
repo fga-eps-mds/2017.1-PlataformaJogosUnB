5c6e74f4e2e87185280bfa40ff92a9ab8cd4f4a0
Icaro Pires <icaropiresdf@hotmail.com>
2017-04-06 18:48:23 +0000

    Import model Information to Game and move validators to a new file
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- funbox/game/models.py
+++ funbox/game/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
-import re
-
-def validate_version(version):
-	version_pattern = '^(\d+\.)*\d+$'
-	if re.match(version_pattern, version) is None:
-		raise ValidationError(
-			_("The version characters can only be or a  '.' or a digit and can't have 2 follows '.'. Error at: %(version)s"),
-			params={'version': version},
-		)
+from game.validators import *
+from information.models import Information
 
 class Game(models.Model):
 	name =  models.CharField(
@@ -18,16 +11,18 @@ class Game(models.Model):
 		null=False,
 		blank=False,
 		help_text=_("What's the name of the game?")
-	)	
+	)
 
 	game_version = models.CharField(
 		_('Game version'),
-                max_length=20,
+		max_length=20,
 		validators=[validate_version],
-                null=True,
-                blank=True,
-                help_text=_("What's the game version")
+		null=True,
+		blank=True,
+		help_text=_("What's the game version")
         )
+
+	information = models.ForeignKey(Information)
 	
 	def save(self, *args, **kwargs):
 		self.clean_fields()
@@ -35,20 +30,4 @@ class Game(models.Model):
 
 	def __str__(self):
 		return self.name
-
-class Information(models.Model):
-        description = models.TextField(
-		_('Description'),
-                max_length=1000,
-                null=False,
-                blank=False,
-                help_text=_("Describe your game")
-        )
-
-        launch_year = models.IntegerField(
-		_('Launch year'),
-                null=False,
-                blank=False,
-                help_text=_("Which was the year the game was launched?")
-        )
 
