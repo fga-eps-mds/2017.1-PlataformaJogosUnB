c6d15b55812889b89492806dbc0d10ab2dc606e5
Vagrant Default User <vagrant@jessie.raw>
2017-04-08 02:19:30 +0000

    Test if game is save on data base

--- funbox/game/test_models.py
+++ funbox/game/test_models.py
@@ -23,3 +23,14 @@ class TestBasicdb:
     def test_my_first(self,game_created):
         game = Game.objects.get(pk=1)
         assert game_created == game
+
+@pytest.mark.django_db
+def test_game():
+    Information(description='feef', launch_year=2017).save()
+    game = Game(name="HELLO",
+                game_version="1.0",
+                information_id=1)
+    game.save()
+    game_from_db=Game.objects.get(pk=1)
+    assert game == game_from_db
+
