cce3c337985e31b683aa0c8edbf5d3e63f0611d6
Icaro Pires <icaropiresdf@hotmail.com>
2017-04-08 02:32:41 +0000

    Format game models and game validators in pep8
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- funbox/game/models.py
+++ funbox/game/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from game.validators import *
+from game.validators import validate_version
 from information.models import Information
 
 
--- funbox/game/validators.py
+++ funbox/game/validators.py
@@ -2,11 +2,12 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 import re
 
-def validate_version(version):
-	version_pattern = '^(\d+\.)*\d+$'
-	if re.match(version_pattern, version) is None:
-		raise ValidationError(
-			_("The version characters can only be or a  '.' or a digit and can't have 2 follows '.'. Error at: %(version)s"),
-			params={'version': version},
-		)
 
+def validate_version(version):
+    version_pattern = '^(\d+\.)*\d+$'
+    if re.match(version_pattern, version) is None:
+        raise ValidationError(
+            _("The version characters can only be or a  '.' or a digit and" +
+                "can't have 2 follows '.'. Error at: % (version)s"),
+            params={'version': version},
+        )
