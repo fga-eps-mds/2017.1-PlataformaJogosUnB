81d66155c9bdcb30e1855838f3a85f28a2a973a9
Icaro Pires <icaropiresdf@hotmail.com>
2017-04-08 04:28:21 +0000

    Create Award and Statistic model classes of Information model
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- funbox/information/models.py
+++ funbox/information/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.core.validators import MinValueValidator
 
 
 class Information(models.Model):
+
     description = models.TextField(
         _('Description'),
         max_length=1000,
@@ -11,8 +13,9 @@ class Information(models.Model):
         help_text=_("Describe your game.")
     )
 
-    launch_year = models.IntegerField(
+    launch_year = models.PositiveInteger(
         _('Launch year'),
+        max_length=4,
         null=False,
         blank=False,
         help_text=_("Which was the year the game was launched?")
@@ -21,3 +24,55 @@ class Information(models.Model):
     def save(self, *args, **kwargs):
         self.clean_fields()
         super(Information, self).save(*args, **kwargs)
+
+
+class Statistic(models.Model):
+
+    download_amount = models.BigIntegerField(
+        _('Dowload amount'),
+        null=False,
+        blank=False,
+        help_text=_("Game amount of downloads")
+    )
+
+    access_amount = models.BigIntegerField(
+        _('Access amount'),
+        null=False,
+        blank=False,
+        help_text=_("Game amount of access")
+    )
+
+    def save(self, *args, **kwargs):
+        self.clean_fields()
+        super(Statistic, self).save(*args, **kwargs)
+
+
+class Award(models.Model):
+
+    name = models.CharField(
+        _('Name'),
+        max_length=100,
+        null=False,
+        blank=False,
+        help_text=_("Name of the award")
+    )
+
+    year = models.PositiveInteger(
+        _('Year'),
+        max_length=4,
+        null=False,
+        blank=False,
+        help_text=_("Year of the ward")
+    )
+
+    place = models.CharField(
+        _('Place'),
+        max_length=100,
+        null=False,
+        blank=False,
+        help_text=_("Place whose game won the award")
+    )
+
+    def save(self, *args, **kwargs):
+        self.clean_fields()
+        super(Award, self).save(*args, **kwargs)
