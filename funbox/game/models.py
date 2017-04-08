7a5203cfd1ea7bf8f72f24a3f18fb0fc87925abd
Victor Leite <victorsfleite@gmail.com>
2017-04-08 00:44:00 -0300

    Change relationship between 'Game' and 'Information' to One-to-One and apply pep8 to code.
    Signed-off-by: Victor Leite <victorsfleite@gmail.com>

--- funbox/game/models.py
+++ funbox/game/models.py
@@ -1,33 +1,32 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 from game.validators import *
 from information.models import Information
 
+
 class Game(models.Model):
-	name =  models.CharField(
-		_('Name'),
-		max_length=100,
-		null=False,
-		blank=False,
-		help_text=_("What's the name of the game?")
-	)
+    name = models.CharField(
+        _('Name'),
+        max_length=100,
+        null=False,
+        blank=False,
+        help_text=_("What's the name of the game?")
+    )
 
-	game_version = models.CharField(
-		_('Game version'),
-		max_length=20,
-		validators=[validate_version],
-		null=True,
-		blank=True,
-		help_text=_("What's the game version")
-        )
+    game_version = models.CharField(
+        _('Game version'),
+        max_length=20,
+        validators=[validate_version],
+        null=True,
+        blank=True,
+        help_text=_("What's the game version?")
+    )
 
-	information = models.ForeignKey(Information)
-	
-	def save(self, *args, **kwargs):
-		self.clean_fields()
-		super(Game, self).save(*args, **kwargs)
+    information = models.OneToOneField(Information)
 
-	def __str__(self):
-		return self.name
+    def save(self, *args, **kwargs):
+        self.clean_fields()
+        super(Game, self).save(*args, **kwargs)
 
+    def __str__(self):
+        return self.name
--- funbox/information/models.py
+++ funbox/information/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
 class Information(models.Model):
-	description = models.TextField(
-		_('Description'),
-		max_length=1000,
-		null=False,
-		blank=False,
-		help_text=_("Describe your game")
-	)
+    description = models.TextField(
+        _('Description'),
+        max_length=1000,
+        null=False,
+        blank=False,
+        help_text=_("Describe your game.")
+    )
 
-	launch_year = models.IntegerField(
-		_('Launch year'),
-		null=False,
-		blank=False,
-		help_text=_("Which was the year the game was launched?")
-)
+    launch_year = models.IntegerField(
+        _('Launch year'),
+        null=False,
+        blank=False,
+        help_text=_("Which was the year the game was launched?")
+    )
 
-	def save(self, *args, **kwargs):
-		self.clean_fields()
-		super(Information, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        self.clean_fields()
+        super(Information, self).save(*args, **kwargs)
