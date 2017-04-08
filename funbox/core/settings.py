c384ddc6d93c555d518bfc98093ef8ed289a8f22
Marcelo Ferreira <marcelohpf@gmail.com>
2017-04-08 16:41:48 -0300

    Create base struct for templates
    Signed-off-by: Varley Silva <varleysilva@me.com>
    Signed-off-by: Rafael Brangaca <rabragancaunb@gmail.com>
    Signed-off-by: Vinicius Lima <vfbdelima@gmail.com>
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- funbox/core/settings.py
+++ funbox/core/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'core/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'core/templates'),os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
