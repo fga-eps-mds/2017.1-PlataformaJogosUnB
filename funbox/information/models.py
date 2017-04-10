7069384329fb37a3443f2596b702ae17fac96f7f
Victor Leite <victorsfleite@gmail.com>
2017-04-10 18:14:27 -0300

    Add 'Developer' class to the Information app.
    Signed-off-by: Victor Leite <victorsfleite@gmail.com>

--- funbox/information/models.py
+++ funbox/information/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinValueValidator
+# from .models import *
 
 
 class Information(models.Model):
@@ -10,14 +11,14 @@ class Information(models.Model):
         max_length=1000,
         null=False,
         blank=False,
-        help_text=_("Describe your game.")
+        help_text=_("Describe the game.")
     )
 
     launch_year = models.PositiveIntegerField(
-        _('Launch year'),
+        _('Launch Semester/Year'),
         null=False,
         blank=False,
-        help_text=_("Which was the year the game was launched?"),
+        help_text=_("Which was the semester/year the game was launched?"),
     )
 
     def save(self, *args, **kwargs):
@@ -27,18 +28,22 @@ class Information(models.Model):
 
 class Statistic(models.Model):
 
-    download_amount = models.BigIntegerField(
-        _('Dowload amount'),
-        null=False,
-        blank=False,
-        help_text=_("Game amount of downloads")
+    information = models.ForeignKey(
+        Information
     )
 
-    access_amount = models.BigIntegerField(
-        _('Access amount'),
+    downloads_amount = models.BigIntegerField(
+        _('Dowloads Amount'),
         null=False,
         blank=False,
-        help_text=_("Game amount of access")
+        help_text=_("Game amount of downloads.")
+    )
+
+    accesses_amount = models.BigIntegerField(
+        _('Accesses Amount'),
+        null=False,
+        blank=False,
+        help_text=_("Game amount of access.")
     )
 
     def save(self, *args, **kwargs):
@@ -53,14 +58,14 @@ class Award(models.Model):
         max_length=100,
         null=False,
         blank=False,
-        help_text=_("Name of the award")
+        help_text=_("Name of the award.")
     )
 
     year = models.PositiveIntegerField(
         _('Year'),
         null=False,
         blank=False,
-        help_text=_("Year of the ward")
+        help_text=_("Year the award was won.")
     )
 
     place = models.CharField(
@@ -68,9 +73,50 @@ class Award(models.Model):
         max_length=100,
         null=False,
         blank=False,
-        help_text=_("Place whose game won the award")
+        help_text=_("Place where the game won the award.")
     )
 
     def save(self, *args, **kwargs):
         self.clean_fields()
         super(Award, self).save(*args, **kwargs)
+
+
+class Developer(models.Model):
+
+    name = models.CharField(
+        _('Name'),
+        max_length=100,
+        null=False,
+        blank=False,
+        help_text=_("Name of the person who developed the game.")
+    )
+
+    avatar = models.FileField(
+        _('Image'),
+        null=False,
+        blank=True,
+        help_text=_("Developer image.")
+    )
+
+    login = models.CharField(
+        _('Login'),
+        max_length=50,
+        null=False,
+        blank=False,
+        help_text=_("Developer login for github.")
+    )
+
+    email = models.EmailField(
+        _('E-mail'),
+        max_length=100,
+        null=True,
+        blank=True,
+        help_text=_("Developer e-mail contact.")
+    )
+
+    github_page = models.URLField(
+        _('Github Page'),
+        null=False,
+        blank=False,
+        help_text=_("Developer Github page.")
+    )
