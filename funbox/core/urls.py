26ca5c8555c4a4ac6ec8ec21293480eb236984ed
Victor Leite <victorsfleite@gmail.com>
2017-04-12 04:24:00 -0300

    Organize the core urls configuration.
    Signed-off-by: Victor Leite <victorsfleite@gmail.com>

--- funbox/core/urls.py
+++ funbox/core/urls.py
@@ -4,22 +4,23 @@ from django.contrib.auth.views import (
     password_reset, password_reset_done, password_reset_confirm,
     password_reset_complete,
 )
-from core import views
-from game import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from core import views
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^admin/', admin.site.urls),
     url(r'^admin/reset-password/$', password_reset, name='reset_password'),
     url(r'^admin/reset-password/done/$',
         password_reset_done, name='password_reset_done'),
-    url(r'^admin/reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        password_reset_confirm, name='password_reset_confirm'),
+    url(r'^admin/reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)' +
+        '-(?P<token>.+)/$', password_reset_confirm,
+        name='password_reset_confirm'),
     url(r'^admin/reset-password/complete/$',
         password_reset_complete, name='password_reset_complete'),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^games/', views.GameList.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls',
+        namespace='rest_framework')),
+    url(r'^games/', include('game.urls', namespace='game')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
