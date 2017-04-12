622181349a476329b219d9e984632f1cc1e48c4a
leticiadesouza <leticia.souza212@gmail.com>
2017-04-12 13:06:01 -0300

    Split validations tests of the information
    Signed-off-by: leticiadesouza <leticia.souza212@gmail.com>

--- funbox/information/test_models.py
+++ funbox/information/test_models.py
@@ -1,18 +1,18 @@
 import unittest
 import pytest
+from core.helper_test import validation_test, mount_error_dict, ErrorMessage
 from game.models import Game
 from information.models import Information
 
 def information_creation(description="", launch_year=0):
     return Information(description=description, launch_year=launch_year)
 
-def mount_error_dict(keys, values):
-    return dict([x for x in zip(keys, values)])
 
 def now():
     from datetime import datetime
     year = datetime.now().year
     return year
+
 
 @pytest.fixture
 def information_created():
@@ -33,9 +33,6 @@ class TestInformationCreation:
         assert information_created == information
 
 class TestInformationValidation:
-    error_message_null = 'Este campo não pode ser nulo.'
-    error_message_blank = "Este campo não pode estar vazio."
-    error_not_a_integer = "'' valor deve ser um inteiro."
     error_message_min_value = "A game description must have at least 50 \
 characters!"
     error_launch_year_past = 'Our University had not been built at this\
@@ -46,22 +43,26 @@ characters!"
 
     @pytest.mark.django_db
     @pytest.mark.parametrize("information, errors_dict", [
-        (information_creation(description=None, launch_year=2017),
-        mount_error_dict(["description"], [[error_message_null]])),
-        (information_creation(description="", launch_year=2017),
-        mount_error_dict(["description"], [[error_message_blank]])),
-        (information_creation(description=short_description, launch_year=2017),
-        mount_error_dict(["description"], [[error_message_min_value]])),
+        (information_creation(description=description, launch_year=None),
+        mount_error_dict(["launch_year"], [[ErrorMessage.NULL]])),
         (information_creation(description=description, launch_year=""),
-        mount_error_dict(["launch_year"], [[error_not_a_integer]])),
+        mount_error_dict(["launch_year"], [[ErrorMessage.NOT_INTEGER]])),
         (information_creation(description=description, launch_year=1961),
         mount_error_dict(["launch_year"], [[error_launch_year_past]])),
         (information_creation(description=description, launch_year=now()+1),
         mount_error_dict(["launch_year"], [[error_launch_year_future]])),
     ])
-    def test_description_shoudnt_be_null(self, information, errors_dict):
-        from django.core.exceptions import ValidationError
-        with pytest.raises(ValidationError)as validation_error:
-            information.save()
-        print(dict(validation_error.__dict__))
-        assert validation_error.value.message_dict == errors_dict
+    def test_launch_year_validation(self, information, errors_dict):
+        validation_test(information, errors_dict)
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("information, errors_dict", [
+        (information_creation(description=None, launch_year=2017),
+        mount_error_dict(["description"], [[ErrorMessage.NULL]])),
+        (information_creation(description="", launch_year=2017),
+        mount_error_dict(["description"], [[ErrorMessage.BLANK]])),
+        (information_creation(description=short_description, launch_year=2017),
+        mount_error_dict(["description"], [[error_message_min_value]])),
+    ])
+    def test_description_validation(self, information, errors_dict):
+        validation_test(information, errors_dict)
