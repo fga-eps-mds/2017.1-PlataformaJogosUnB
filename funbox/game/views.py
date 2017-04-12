bc523a6901464881efca4d2e33e8362ef50a4aee
= <thiagoflames@gmail.com>
2017-04-12 01:56:06 +0000

    Make serialize of game

--- funbox/game/views.py
+++ funbox/game/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import Game
+from .serializers import GameSerializer
 
-# Create your views here.
+
+class GameList(APIView):
+
+    def get(self, request):
+        games = Game.objects.all()
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data)
+#def listGames(request):
+ #   return render(request, 'game_list.html', {})
