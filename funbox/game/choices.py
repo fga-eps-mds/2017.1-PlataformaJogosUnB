8aa3675fe57eca498afccb1d8e72cf60cf092792
Icaro Pires <icaropiresdf@hotmail.com>
2017-04-14 21:41:56 +0000

    Build Platform model
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

---
+++ funbox/game/choices.py
@@ -0,0 +1,8 @@
+from django.utils.translation import ugettext_lazy as _
+
+EXTENSION_CHOICES = (
+    (1, _('deb')),
+    (2, _('exe')),
+    (3, _('rpm')),
+    (4, _('app')),
+)
--- funbox/game/models.py
+++ funbox/game/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import URLValidator
 from game.validators import validate_version
+from game.choices import EXTENSION_CHOICES
 
 KILOBYTE = 1024
-MAX_UPLOAD_SIZE = 1 * KILOBYTE ** 2
+# MAX_UPLOAD_SIZE = 1 * KILOBYTE ** 3
 
-#TODO: fix MAX_UPLOAD_SIZE validation - save as a string or define a constant.
+# TODO: fix MAX_UPLOAD_SIZE validation - save as a string or define a constant.
+
 
 class Game(models.Model):
 
@@ -44,21 +46,50 @@ class Game(models.Model):
         return "%s v%s" % (self.name, self.game_version)
 
 
+class Platform(models.Model):
+
+    name = models.CharField(
+        _('Package name'),
+        max_length=50,
+        null=False,
+        blank=False,
+        help_text=('Name of the game\'s package'),
+    )
+
+    extensions = models.CharField(
+        _('Valid extension'),
+        max_length=3,
+        choices=EXTENSION_CHOICES,
+        null=False,
+        blank=False,
+        help_text=('Select the package extension that will be accepted'),
+    )
+
+    def save(self, *args, **kwargs):
+        self.clean_fields()
+        super(Platform, self).save(*args, **kwargs)
+
+
 class Package(models.Model):
 
     package = models.FileField(
         _('Package'),
         upload_to='public/packages',
-        max_length=MAX_UPLOAD_SIZE,
+        # max_length=MAX_UPLOAD_SIZE,
         null=False,
         blank=False,
-        help_text=('Choose the game\'s package'),
+        help_text=('Choose the game\'s package')
     )
 
     games = models.ForeignKey(
         Game,
         on_delete=models.CASCADE,
         related_name='packages'
+    )
+
+    platform = models.ManyToManyField(
+        Platform,
+        related_name='platforms'
     )
 
     def save(self, *args, **kwargs):
