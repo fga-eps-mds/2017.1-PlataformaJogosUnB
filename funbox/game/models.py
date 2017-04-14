a5779b2bb3b505078c93d094998989918d55dd5b
leticiadesouza <leticia.souza212@gmail.com>
2017-04-14 14:35:03 -0300

    Build Package class
    Signed-off-by: leticiadesouza <leticia.souza212@gmail.com>

--- funbox/game/models.py
+++ funbox/game/models.py
@@ -3,6 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 from django.core.validators import URLValidator
 from game.validators import validate_version
 
+KILOBYTE = 1024
+MAX_UPLOAD_SIZE = 1 * KILOBYTE ** 3
+
 
 class Game(models.Model):
 
@@ -31,16 +34,26 @@ class Game(models.Model):
         help_text=_('What is the official repository for this game?'),
     )
 
-
     def save(self, *args, **kwargs):
         self.clean_fields()
         super(Game, self).save(*args, **kwargs)
+
     def __str__(self):
         pass
         return "%s v%s" % (self.name, self.game_version)
 
 
 class Package(models.Model):
+
+    package = models.FileField(
+        _('Package'),
+        max_length=MAX_UPLOAD_SIZE,
+        null=False,
+        blank=False,
+        help_text=('Choose the game\'s package'),
+        allow_empty_file=False,
+    )
+
     def save(self, *args, **kwargs):
         self.clean_fields()
         super(Package, self).save(*args, **kwargs)
