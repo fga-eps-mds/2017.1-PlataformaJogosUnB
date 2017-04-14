a7ae84c43e77c386bd7f0c2da91abef974728216
Icaro Pires <icaropiresdf@hotmail.com>
2017-04-14 21:43:24 +0000

    Put Platform and Package inline
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- funbox/game/admin.py
+++ funbox/game/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Game, Package
+from .models import Game, Package, Platform
 from information.models import Information
+from .choices import EXTENSION_CHOICES
 
 
 class InformationInline(admin.StackedInline):
@@ -9,10 +10,28 @@ class InformationInline(admin.StackedInline):
 
 class PackageInline(admin.StackedInline):
     model = Package
+    extra = 0
+    min_num = 1
+    max_num = len(EXTENSION_CHOICES)
+
+
+class PlatformsInline(admin.StackedInline):
+    model = Package.platform.through
+    exclude = ('platforms', )
+    extra = 0
+    min_num = 1
+    max_num = len(EXTENSION_CHOICES)
+
+
+class PackageAdmin(admin.ModelAdmin):
+    inlines = [PlatformsInline, ]
+    exclude = ('platforms', )
 
 
 class GameAdmin(admin.ModelAdmin):
-    inlines = [InformationInline, PackageInline]
+    inlines = [InformationInline, PackageInline, ]
+
 
 admin.site.register(Game, GameAdmin)
-admin.site.register(Package)
+admin.site.register(Package, PackageAdmin)
+admin.site.register(Platform)
