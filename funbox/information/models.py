9e8e15c15a9a14bceddc37e0b6ef16d68df76608
Victor Leite <victorsfleite@gmail.com>
2017-04-14 23:39:53 -0300

    Configure permissions on the app.
    Signed-off-by: Victor Leite <victorsfleite@gmail.com>

--- funbox/information/models.py
+++ funbox/information/models.py
@@ -143,7 +143,8 @@ class Information(models.Model):
 
     awards = models.ManyToManyField(
         Award,
-        related_name='awards'
+        related_name='awards',
+        blank=True
     )
 
     def save(self, *args, **kwargs):
