497e67c4218172d8dfcea82585628225e09b65f3
leticiadesouza <leticia.souza212@gmail.com>
2017-04-15 15:57:10 -0300

    Fix index names for extension_choices
    Signed-off-by: leticiadesouza <leticia.souza212@gmail.com>

--- funbox/game/choices.py
+++ funbox/game/choices.py
@@ -1,8 +1,8 @@
 from django.utils.translation import ugettext_lazy as _
 
 EXTENSION_CHOICES = (
-    (1, _('deb')),
-    (2, _('exe')),
-    (3, _('rpm')),
-    (4, _('app')),
+    ('deb', _('deb')),
+    ('exe', _('exe')),
+    ('rpm', _('rpm')),
+    ('app', _('app')),
 )
