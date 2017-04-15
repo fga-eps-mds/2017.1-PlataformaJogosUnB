dc6ab89813b2eeb82ebac015fd86791a4484a568
Vinicius <vfbdelima@gmail.com>
2017-04-15 19:00:20 -0300

    Adjust to list games in database

--- funbox/core/views.py
+++ funbox/core/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from game.models import Game
 
 def index(request):
-    return render(request, 'game/index.html',{} )
+	queryset = Game.objects.all()
+	return render(request, 'game/index.html', {'games':queryset})
 
 def show(request):
     return render(request, 'game/show.html',{} )
