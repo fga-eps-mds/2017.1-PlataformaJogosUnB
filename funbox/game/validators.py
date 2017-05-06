00ec62d5d494280afdf1c2b8cf5edc1fd48b1a5b
Thiago Pereira <thiagoflames@gmail.com>
2017-05-06 15:00:20 +0000

    Add tests to version validation
    Signed-off-by: Thiago Pereira <thiagoflames@gmail.com>
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- funbox/game/validators.py
+++ funbox/game/validators.py
@@ -7,7 +7,7 @@ def validate_version(version):
     version_pattern = '^(\d+\.)*\d+$'
     if re.match(version_pattern, version) is None:
         raise ValidationError(
-            _("The version characters can only be either a '.' or a digit" +
+            _("The version characters can only be either a '.' or a digit " +
               "and can't have 2 followed '.'. Error at: % (version)s"),
             params={'version': version},
         )
