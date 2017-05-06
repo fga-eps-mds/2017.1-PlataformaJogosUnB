91493daae802d34f9d5bf493a0b9d2a4a2fefa48
Marcelo Ferreira <marcelohpf@gmail.com>
2017-05-06 13:21:57 +0000

    Add more tests to views game set
    Signed-off-by: Thiago Pereira <thiagoflames@gmail.com>
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- funbox/game/test_views.py
+++ funbox/game/test_views.py
@@ -1,4 +1,5 @@
 import pytest
+from game.serializers import GameSerializer
 from game.models import Game
 
 
@@ -10,7 +11,7 @@ def create_game():
     return game
 
 
-class TestGameList:
+class TestGameViewSet:
 
     @pytest.mark.django_db
     def test_header_game_list(self, client, create_game):
@@ -27,9 +28,6 @@ class TestGameList:
         assert len(response.data["games"]) == 1
         assert response.data["games"].first() == create_game
 
-
-class TestGameDetail:
-
     @pytest.mark.django_db
     def test_header_game_detail(self, client, create_game):
         response = client.get("/games/detail/{}/".format(create_game.pk))
@@ -37,3 +35,17 @@ class TestGameDetail:
         assert response.template_name == 'game/show.html'
         assert response.get("Content-Type") == 'text/html; charset=utf-8'
         assert response.charset == 'utf-8'
+
+    @pytest.mark.django_db
+    def test_data_game_detail(self, client, create_game):
+        response = client.get("/games/detail/{}/".format(create_game.pk))
+        assert "game" in response.data
+        assert response.data["game"] == create_game
+
+    @pytest.mark.django_db
+    def test_game_list_json(self, client, create_game):
+        response = client.get("/games/list.json")
+        assert response.status_code == 200
+        assert response.get("Content-Type") == 'application/json'
+        serializer = GameSerializer(Game.objects.all(), many=True)
+        assert response.data == serializer.data
