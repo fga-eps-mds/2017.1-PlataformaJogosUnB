1e7a912f542fa8cba376835abed8a2ad5c3d3b78
Thiago <thiagoflames@gmail.com>
2017-05-11 12:35:29 -0300

    Add search field
    Signed-off-by: Thiago <thiagoflames@gmail.com>

--- funbox/information/admin.py
+++ funbox/information/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from information.models import Information, Developer, Award, Genre
 
-
 class AwardsInline(admin.TabularInline):
     model = Information.awards.through
 
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ['name', 'year', 'place', ]
+    search_fields = ['name', 'year', 'place', ]
+
 class DevelopmentsInline(admin.TabularInline):
     model = Information.developers.through
+
+class DevelopmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', ]
+    search_fields = ['name', 'email', ]
 
 class GenresInline(admin.TabularInline):
     model = Information.genres.through
@@ -19,6 +26,6 @@ class InformationAdmin(admin.ModelAdmin):
     inlines = [AwardsInline, DevelopmentsInline, GenresInline, ]
     exclude = ('developers', 'awards', 'genres')
 
-admin.site.register(Developer)
-admin.site.register(Award)
+admin.site.register(Developer, DevelopmentAdmin)
+admin.site.register(Award, AwardAdmin)
 admin.site.register(Genre, GenreAdmin)
