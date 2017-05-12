fef012a65f20cb1a11b406cfa805c1549f503829
Vinicius <vfbdelima@gmail.com>
2017-05-12 04:30:01 +0000

    Fix multiple files saving on admin
    Signed-off-by: Vinicius <vfbdelima@gmail.com>
    Signed-off-by: Joao Robson <joaorobsonmartins@gmail.com>

--- funbox/media/admin.py
+++ funbox/media/admin.py
@@ -2,17 +2,43 @@ from media.models import Image, Video, Soundtrack
 from django.contrib import admin
 from media.forms import ImageForm, VideoForm, SoundtrackForm
 
-
 class ImageAdmin(admin.ModelAdmin):
     form = ImageForm
 
+    def save_model(self, request, obj, form, change):
+        list_images = request.FILES.getlist('image')
+        save_object(instance=obj, list_file=list_images)
 
 class VideoAdmin(admin.ModelAdmin):
     form = VideoForm
 
+    def save_model(self, request, obj, form, change):
+        list_videos = request.FILES.getlist('video')
+        save_object(instance=obj, list_file=list_videos)
+
 
 class SoundtrackAdmin(admin.ModelAdmin):
     form = SoundtrackForm
+
+    def save_model(self, request, obj, form, change):
+        list_soundtrack = request.FILES.getlist('soundtrack')
+        save_object(instance=obj, list_file=list_soundtrack)
+
+def save_object(instance, list_file):
+    game = instance.game
+    role = instance.role
+    if type(instance) == Image:
+        for archive in list_file:
+            instance = type(instance).objects.create(game=game,role=role,image=archive)
+            instance.save()
+    elif type(instance) == Video:
+        for archive in list_file:
+            instance = type(instance).objects.create(game=game,role=role,video=archive)
+            instance.save()
+    elif type(instance) == Soundtrack:
+        for archive in list_file:
+            instance = type(instance).objects.create(game=game,role=role,soundtrack=archive)
+            instance.save()
 
 admin.site.register(Image, ImageAdmin)
 admin.site.register(Video, VideoAdmin)
