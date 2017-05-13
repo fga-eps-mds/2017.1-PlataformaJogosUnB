c258f0f37740eb177e6edb01a36ecbc1af9f7346
Eduardo Nunes <eduardonunes2525@gmail.com>
2017-05-13 16:16:29 -0300

    Modify tests for pytest
    Signed-off-by: Eduardo Nunes <eduardonunes2525@gmail.com>
    Signed-off-by: Thiago <thiagoflames@gmail.com>

--- funbox/information/test_serializers.py
+++ funbox/information/test_serializers.py
@@ -1,53 +1,60 @@
-from django.test import TestCase
-from information.models import Award, Developer, Genre
+from information.serializers import InformationSerializer
+from information.models import Award, Developer, Genre, Information
 from information.serializers import GenreSerializer
 from information.serializers import DeveloperSerializer
 from information.serializers import AwardSerializer
+import pytest
 
 
-class GenreSerializerTestCase(TestCase):
+class TestGenreSerializer:
 
+    @pytest.mark.django_db
     def test_serialization_genre(self):
         genre = Genre(
-            name='Race',
-            description='Games like fast and furious')
+                name='Corrida',
+                description='Jogos de carros velozes')
         genre.save()
+
         genre_serialized = GenreSerializer(genre).data
         expected_json_genre = {
-            'name': 'Race',
-            'description': 'Games like fast and furious'}
-        self.assertEqual(genre_serialized, expected_json_genre)
+                'name': 'Corrida',
+                'description': 'Jogos de carros velozes'}
+        assert genre_serialized == expected_json_genre
 
 
-class AwardSerializerTestCase(TestCase):
+class TestAwardSerializer:
 
+    @pytest.mark.django_db
     def test_serialization_award(self):
         award = Award(name='VGA', year='2017', place='UnB')
         award.save()
 
         award_serialized = AwardSerializer(award).data
         expected_json_award = {
-            "name": 'VGA',
-            "year": 2017,
-            "place": 'UnB'}
-        self.assertEqual(award_serialized, expected_json_award)
+                 "name": 'VGA',
+                 "year": 2017,
+                 "place": 'UnB'}
+        assert award_serialized == expected_json_award
 
 
-class DeveloperSerializerTestCase(TestCase):
+class TestDeveloperSerializer:
 
+    @pytest.mark.django_db
     def test_serialization_developer(self):
         developer = Developer(
-            name='Developer',
-            avatar='none',
-            login='developer',
-            email='developer@gmail.com',
-            github_page='http://github.com/developer')
+                name='Developer',
+                avatar='none',
+                login='developer',
+                email='developer@gmail.com',
+                github_page='http://github.com/developer')
         developer.save()
+
         developer_serialized = DeveloperSerializer(developer).data
         expected_json_developer = {
-            'name': 'Developer',
-            'avatar': '/public/none',
-            'login': 'developer',
-            'email': 'developer@gmail.com',
-            'github_page': 'http://github.com/developer'}
-        self.assertEqual(developer_serialized, expected_json_developer)
+                'name': 'Developer',
+                'avatar': '/public/none',
+                'login': 'developer',
+                'email': 'developer@gmail.com',
+                'github_page': 'http://github.com/developer'}
+        assert developer_serialized == expected_json_developer
+
