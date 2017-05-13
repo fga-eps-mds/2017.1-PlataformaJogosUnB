bcca837aae65e971207ab78a8c8209587fa990b4
Vinicius <vfbdelima@gmail.com>
2017-05-13 19:15:17 +0000

    Multiple upload files on inline corrected
    Signed-off-by: Vinicius <vfbdelima@gmail.com>
    Signed-off-by: Joao Robson <joaorobsonmartins@gmail.com>

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -3,7 +3,7 @@ from .models import Game, Package, Platform
 from information.models import Information
 from media.models import Image, Video, Soundtrack
 from .choices import EXTENSION_CHOICES
-from media.forms import ImageForm
+from media.forms import ImageForm, VideoForm, SoundtrackForm
 
 
 class InformationInline(admin.StackedInline):
@@ -21,13 +21,14 @@ class VideoInline(admin.StackedInline):
     model = Video
     extra = 0
     min_num = 0
+    form = VideoForm
 
 
 class SoundtrackInline(admin.StackedInline):
     model = Soundtrack
     extra = 0
     min_num = 0
-
+    form = SoundtrackForm
 
 class PackageInline(admin.StackedInline):
     model = Package
@@ -44,14 +45,33 @@ class PlatformsInline(admin.StackedInline):
     max_num = len(EXTENSION_CHOICES)
 
 
+class PackageAdmin(admin.ModelAdmin):
+    exclude = ['platforms']
+
+
 class GameAdmin(admin.ModelAdmin):
     inlines = [InformationInline, PackageInline,
                ImageInline, VideoInline, SoundtrackInline]
 
 
-class PackageAdmin(admin.ModelAdmin):
-    exclude = ['platforms']
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
 
+        if type(instances[0]) == Image:
+            for count, forms in enumerate(formset):
+                list_files = request.FILES.getlist('media_image-'+str(count)+'-image')
+                forms.save_instances(list_files)
+        elif type(instances[0]) == Video:
+            for count, forms in enumerate(formset):
+                list_files = request.FILES.getlist('media_video-'+str(count)+'-video')
+                forms.save_instances(list_files)
+        elif type(instances[0]) == Soundtrack:
+            for count, forms in enumerate(formset):
+                list_files = request.FILES.getlist('media_soundtrack-'+str(count)+'-soundtrack')
+                forms.save_instances(list_files)
+        else:
+            for instance in instances:
+                instance.save()
 
 admin.site.register(Game, GameAdmin)
 admin.site.register(Platform)
