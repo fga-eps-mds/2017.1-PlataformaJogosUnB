fe297e1d65767f265df5cbd2467a5f931262bc58
Thiago Pereira <thiagoflames@gmail.com>
2017-05-14 01:22:38 +0000

    Create the test for file size validation and add mock for file
    Signed-off-by: Thiago Pereira <thiagoflames@gmail.com>
    Signed-off-by: Eduardo Nunes <eduardonunes2525@gmail.com>
    Signed-off-by: Marcelo Herton <marcelohpf@gmail.com>

--- funbox/game/validators.py
+++ funbox/game/validators.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
-'''from django.template.defaultfilters import filesizeformat'''
+from django.template.defaultfilters import filesizeformat
 import re
 
 
