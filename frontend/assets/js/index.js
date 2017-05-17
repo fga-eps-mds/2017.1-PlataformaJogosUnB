fce3c1841d41e1d047f36768940cafc46bccea0a
Marcelo Ferreira <marcelohpf@gmail.com>
2017-05-17 18:44:21 +0000

    Make merge between react and django.
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

---
+++ frontend/assets/js/index.js
@@ -0,0 +1,68 @@
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var GameList = React.createClass({
+    loadGamesFromServer: function(){
+        fetch(this.props.url, 
+            { 
+                headers: new Headers({ "Content-Type": "application/json", "Accept": "application/json"}),
+                method: "GET",
+            })
+        .then(function(response){
+             return response.json(); 
+            })
+        .then(function(games){
+            games.forEach(function(e){console.log(e.name)});
+            this.setState({data: games});
+        }.bind(this))
+        .catch(function(error){
+            console.error(error);
+        });
+
+},
+
+getInitialState: function(){
+    return {data: []};
+},
+
+componentDidMount: function(){
+    this.loadGamesFromServer();
+    setInterval(this.loadGamesFromServer, this.props.pollInterval)
+},
+render: function() {
+        if (this.state.data) {
+            console.log('DATA!', this.state.data);
+                return ( 
+
+                    <div>            
+                      <table className="pure-table">
+                        <thead>
+                          <tr>
+                            <th>Nome</th>
+                            <th>Versão</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {
+                            this.state.data.map(function(game){
+                              return (
+                                <tr key={game.id}>
+                                  <td>{game.name}</td>
+                                  <td>{game.game_version}</td>
+                                </tr>
+                              );
+                            })
+                          }
+                        </tbody>
+                      </table> 
+                    </div>    
+                )
+        }
+    }
+})
+
+
+
+ReactDOM.render(<GameList url='/games/list' pollInterval={1000}/>,document.getElementById('container'))
