6efc0144007ba5e0ffb3f21f7697f45e9964a167
ArthurTemporim <arthurrtl@gmail.com>
2017-05-21 15:00:36 +0000

    Create test of game_activated.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>
    Signed-off-by: Rafael Braganca <rabragancaunb@gmail.com>

--- backend/game/test_models.py
+++ backend/game/test_models.py
@@ -13,12 +13,13 @@ from unittest.mock import patch
 
 def game_creation(name="", cover_image="", url="",
                   launch_year=0, version="1.3.4"):
-    return Game(
-        name=name,
-        cover_image=cover_image,
-        official_repository=url,
-        version=version
-    )
+        return Game(
+            name=name,
+            cover_image=cover_image,
+            official_repository=url,
+            game_activated=True,
+            version=version
+        )
 
 
 @pytest.fixture
@@ -27,6 +28,7 @@ def game_created():
     game.name = 'mario'
     game.cover_image = "image.jpg"
     game.official_repository = 'https://github.com/PlataformaJogosUnb/'
+    game.game_activated = True
     game.save()
     return game
 
