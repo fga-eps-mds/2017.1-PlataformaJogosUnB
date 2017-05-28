11560337d2bd3138dd6a8bb0e30cf9fa3c3fd2d5
Varley <varleysilva@me.com>
2017-05-27 21:03:18 -0300

    Adjust informartions into cards
    Signed-off-by: Varley <varleysilva@me.com>
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- frontend/assets/js/layout/DescriptionCard.js
+++ frontend/assets/js/layout/DescriptionCard.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Card } from 'semantic-ui-react';
 
 export default class DescriptionCard extends React.Component{
+  getVersion(){
+    if (this.props.version != null){
+      return(<p><strong>Versão: </strong>{this.props.version}</p>);
+    }else{
+      return (null);
+    }
+  };
+  
   render(){
-
     return(
-    <h1>Description Card</h1>
+      <Card fluid={true}>
+        <Card.Content>
+          <Card.Header>
+            Sobre o jogo
+          </Card.Header>
+          <Card.Description>   
+            { this.getVersion() }
+            <p><strong>Repositório Oficial: </strong>{this.props.official_repository}</p>
+            <p><strong>Ano de lançamento: </strong>{this.props.launch_year}</p>
+            <p><strong>Gênero: </strong>{this.props.name}</p>
+            <p><strong>Developers: </strong> {this.props.developers.map((developer) => {return developer.name} )}</p>
+          </Card.Description>
+        </Card.Content>
+      </Card>
     ); 
   }
 }
