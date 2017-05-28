dcdca42ed55a10fefa28bd9202f8bda05112dd33
Varley <varleysilva@me.com>
2017-05-27 21:03:18 -0300

    Adjust informartions into cards
    Signed-off-by: Varley <varleysilva@me.com>
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- frontend/assets/js/layout/Game.js
+++ frontend/assets/js/layout/Game.js
@@ -8,7 +8,7 @@ import {Grid, Row, Column} from 'react-cellblock';
 export default class Game extends React.Component{
   constructor(props){
     super(props);
-    this.state = { game: {}};
+    this.state = { game: {information: {developers: []}, packages:[], genres:[]  }};
   }
 
   loadGameFromServer(){
@@ -34,21 +34,28 @@ export default class Game extends React.Component{
   componentDidMount() {
         this.loadGameFromServer();
     }
-
+  
+ 
   render(){
+    
     return (
       <div>
-        <h1>{this.state.game.name} - v{this.state.game.version}</h1>
+        <h1>{this.state.game.name} </h1>
           <Grid>
             <Row>
             <Column width="1/2">
                 <InternalSlider />
             </Column>
             <Column width="1/2"> 
-              <InformationCard />
+              <DescriptionCard 
+                version={ this.state.game.version }
+                official_repository={ this.state.game.official_repository }
+                launch_year={ this.state.game.information.launch_year }
+                developers={this.state.game.information.developers}
+              />
             </Column>
             <Column width="1/2"> 
-                <DescriptionCard />
+              <InformationCard />
             </Column>
            </Row>
           </Grid>
--- frontend/assets/js/layout/InformationCard.js
+++ frontend/assets/js/layout/InformationCard.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
 
-export default class InformationCard extends React.Component{
+export default class InformationCard extends React.Component{  
   render(){
 
     return(
