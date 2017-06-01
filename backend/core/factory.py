92c05f0773a02d5550a9d70021f8fa19ab4da40b
Marcelo Ferreira <marcelohpf@gmail.com>
2017-06-01 00:47:18 -0300

    Enhancements in clean database script
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

---
+++ backend/core/factory.py
@@ -0,0 +1,13 @@
+import factory
+from django.contrib.auth.models import User
+
+
+class UserFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = User
+
+    username = "admin"
+    password = "qwer1234"
+    is_superuser = True
+    is_staff = True
