2a30d69f60d7efd206fffc4bc986ec43e6978248
Marcelo Ferreira <marcelohpf@gmail.com>
2017-06-02 01:08:10 -0300

    Apply codestyle using autofix of pep8
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- backend/information/test_validators.py
+++ backend/information/test_validators.py
@@ -3,7 +3,7 @@ from information.validators import (
     years_validator,
     MIN_IDX,
     MAX_IDX
-  )
+)
 from datetime import datetime
 import pytest
 
@@ -15,22 +15,27 @@ class TimeMock(datetime):
             year = 2017
         return Year()
 
+
 def test_years_validator(mock):
     dt = mock.patch('datetime.datetime')
     dt.now = TimeMock.now
     data = years_validator('MyModel')
     assert data['values'] == (1962, 2017)
     assert data['messages'][0] == 'Our University' \
-    ' had not been built at this time!'
+        ' had not been built at this time!'
     assert data['messages'][1] == 'We believe the MyModel was not ' \
-    'won in the future!'
+        'won in the future!'
+
 
 @pytest.fixture
 def validators():
-    return min_max_validators((10,20),('teste min', 'teste max'))
+    return min_max_validators((10, 20), ('teste min', 'teste max'))
+
+
 def test_min_validator(validators):
     assert validators[MIN_IDX].limit_value == 10
     assert validators[MIN_IDX].message == 'teste min'
+
 
 def test_max_validator(validators):
     assert validators[MAX_IDX].limit_value == 20
