9430c99656e6b80e691bab41f0febe1a481526c1
Vinicius <vfbdelima@gmail.com>
2017-06-03 03:25:31 +0000

    Fix codestyle with eslint
    Signed-off-by: Vinicius <vfbdelima@gmail.com>
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- frontend/assets/js/components/cards/GameCard.js
+++ frontend/assets/js/components/cards/GameCard.js
@@ -3,22 +3,22 @@ import ReactDOM from "react-dom";
 import {Link} from "react-router-dom";
 import {Card, Image} from "semantic-ui-react";
 const cardImageStyle = {
-  background: '#000000',
-  position: 'relative',
-  minHeight: '320px',
-}
+    "background": "#000000",
+    "position": "relative",
+    "minHeight": "320px"
+};
 const imageStyle = {
-  position: 'absolute',
-  top: 0,
-  bottom: 0,
-  right: 0,
-  left: 0,
-  margin: 'auto',
-}
+    "position": "absolute",
+    "top": 0,
+    "bottom": 0,
+    "right": 0,
+    "left": 0,
+    "margin": "auto"
+};
 
 export default class GameCard extends React.Component {
     constructor (props) {
-        
+
         super(props);
 
     }
@@ -28,7 +28,7 @@ export default class GameCard extends React.Component {
         return (
             <Card>
                 <div style={cardImageStyle}>
-                  <Image src={this.props.data.card_image} style={imageStyle} />
+                    <Image src={this.props.data.card_image} style={imageStyle} />
                 </div>
                 <Card.Content>
                     <Card.Header>
