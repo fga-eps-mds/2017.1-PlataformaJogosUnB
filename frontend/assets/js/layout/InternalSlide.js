61f27764dbf085e1ef56f1ab15e17bf44535e9e7
Varley <varleysilva@me.com>
2017-06-03 19:23:58 -0300

    Adjust stylesheet
    Signed-off-by: Varley <varleysilva@me.com>

--- frontend/assets/js/layout/InternalSlide.js
+++ frontend/assets/js/layout/InternalSlide.js
@@ -1,28 +1,34 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ImageGallery from 'react-image-gallery';
-require('style-loader!css-loader!sass-loader!react-image-gallery/styles/scss/image-gallery.scss');
+import React from "react";
+import ReactDOM from "react-dom";
+import ImageGallery from "react-image-gallery";
+require("style-loader!css-loader!sass-loader!react-image-gallery/styles/scss/image-gallery.scss");
 // https://github.com/xiaolin/react-image-gallery
 
-export default class InternalSlider extends React.Component{
+export default class InternalSlider extends React.Component {
 
-    handleImageLoad(event) {
-      console.log('Image loaded ', event.target)
+    handleImageLoad (event) {
+
+        console.log("Image loaded ", event.target);
+
     }
-    render(){
-        var imagesTeste = this.props.data.media_image.map((testando) => {
-            return {original: testando.image, thumbnail: testando.image }
-    });
+    render () {
 
-      return(
-        <ImageGallery
-          items={imagesTeste}
-          slideInterval={2000}
-          onImageLoad={this.handleImageLoad}
-          slideOnThumbnailHover={true}
-          autoplay={true}
-          showPlayButton={false}/>
-      );
+        const imagesTeste = this.props.data.media_image.map((testando) => ({
+            "original": testando.image,
+            "thumbnail": testando.image
+        }));
+
+        return (
+            <ImageGallery
+                items={imagesTeste}
+                slideInterval={2000}
+                onImageLoad={this.handleImageLoad}
+                slideOnThumbnailHover
+                autoplay
+                showPlayButton={false}
+            />
+        );
+
     }
 }
