37a68acff28afcefc06c0c79a38088fa76f0728f
Varley <varleysilva@me.com>
2017-06-03 18:50:04 -0300

    InternalSlide consuming json from backend
    Signed-off-by: Varley <varleysilva@me.com>

--- frontend/assets/js/layout/InternalSlide.js
+++ frontend/assets/js/layout/InternalSlide.js
@@ -1,45 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ImageGallery from "react-image-gallery";
-require("style-loader!css-loader!sass-loader!react-image-gallery/styles/scss/image-gallery.scss");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ImageGallery from 'react-image-gallery';
+require('style-loader!css-loader!sass-loader!react-image-gallery/styles/scss/image-gallery.scss');
 // https://github.com/xiaolin/react-image-gallery
 
-export default class InternalSlider extends React.Component {
+export default class InternalSlider extends React.Component{
 
-    handleImageLoad (event) {
-
-        console.log("Image loaded ", event.target);
-
+    handleImageLoad(event) {
+      console.log('Image loaded ', event.target)
     }
+    render(){
+        var imagesTeste = this.props.data.media_image.map((testando) => {
+            return {original: testando.image, thumbnail: testando.image }
+    });
 
-    render () {
-
-        const images = [
-            {
-                "original": "http://lorempixel.com/1000/600/nature/1/",
-                "thumbnail": "http://lorempixel.com/250/150/nature/1/"
-            },
-            {
-                "original": "http://lorempixel.com/1000/600/nature/2/",
-                "thumbnail": "http://lorempixel.com/250/150/nature/2/"
-            },
-            {
-                "original": "http://lorempixel.com/1000/600/nature/3/",
-                "thumbnail": "http://lorempixel.com/250/150/nature/3/"
-            }
-        ];
-
-
-        return (
-            <ImageGallery
-                items={images}
-                slideInterval={2000}
-                onImageLoad={this.handleImageLoad}
-                slideOnThumbnailHover
-                autoplay
-                showPlayButton={false}
-            />
-        );
-
+      return(
+        <ImageGallery
+          items={imagesTeste}
+          slideInterval={2000}
+          onImageLoad={this.handleImageLoad}
+          slideOnThumbnailHover={true}
+          autoplay={true}
+          showPlayButton={false}/>
+      );
     }
 }
--- frontend/assets/js/pages/Game.js
+++ frontend/assets/js/pages/Game.js
@@ -1,48 +1,32 @@
-import React from "react";
-import InformationCard from "../components/cards/InformationCard.js";
-import DescriptionCard from "../components/cards/DescriptionCard.js";
-import InternalSlide from "../layout/InternalSlide.js";
-import {Card, Container, Grid} from "semantic-ui-react";
+import React from 'react';
+import InformationCard from '../components/cards/InformationCard.js';
+import DescriptionCard from '../components/cards/DescriptionCard.js';
+import InternalSlide from '../layout/InternalSlide.js';
+import { Card, Grid, Container } from 'semantic-ui-react'
 
 
-export default class Game extends React.Component {
-    constructor (props) {
+export default class Game extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { game: {media_image: [], information: {developers: [], awards: [], packages: []}} };
+  }
 
-        super(props);
-        this.state = {
-            "game": {
-                "information": {
-                    "developers": [],
-                    "awards": [],
-                    "packages": []
-                }
-            }
-        };
-
-    }
-
-    loadGameFromServer () {
-
-        console.log(this.props);
-        const id = this.props.match.params.id;
-
-        console.log(id);
-        fetch(`/api/detail/${id}/`,
-            {
-                "headers": new Headers({
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                }),
-                "method": "GET"
-            }).
-        then((response) => response.json()).
-        then((game) => {
-
-            this.setState({game});
-
-        }).
-        catch((error) => {
-
+  loadGameFromServer(){
+    console.log(this.props);
+    const id = this.props.match.params.id;
+    console.log(id)
+        fetch("/api/detail/"+id+"/",
+              {
+                headers: new Headers({ "Content-Type": "application/json", "Accept": "application/json"}),
+                method: "GET",
+            })
+        .then((response) => {
+             return response.json();
+            })
+        .then(((game) => {
+            this.setState({ game: game });
+        }).bind(this))
+        .catch((error) => {
             console.error(error);
 
         });
@@ -55,37 +39,38 @@ export default class Game extends React.Component {
 
     }
 
-    render () {
+  render(){
+console.log(this.state.game.media_image)
+    return (
+      <Container>
+        <h1>{this.state.game.name} </h1>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={10}>
+                  <InternalSlide 
+                  data = { this.state.game }
+                  />
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <DescriptionCard
+                  version={ this.state.game.version }
+                  official_repository={ this.state.game.official_repository }
+                  launch_year={ this.state.game.information.launch_year }
+                />
+              </Grid.Column>
+              </Grid.Row>
 
-        return (
-            <Container>
-                <h1 color="f0f8ff">{this.state.game.name} </h1>
-                <Grid>
-                    <Grid.Row>
-                        <Grid.Column width={10}>
-                            <InternalSlide />
-                        </Grid.Column>
-                        <Grid.Column width={6}>
-                            <DescriptionCard
-                                version={this.state.game.version}
-                                official_repository={this.state.game.official_repository}
-                                launch_year={this.state.game.information.launch_year}
-                            />
-                        </Grid.Column>
-                    </Grid.Row>
-
-                    <Grid.Row>
-                        <Grid.Column width={10}>
-                            <InformationCard
-                                description={this.state.game.information.description}
-                                developers={this.state.game.information.developers}
-                                awards={this.state.game.information.awards}
-                            />
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Container>
-        );
-
-    }
+              <Grid.Row>
+              <Grid.Column width={10}>
+                <InformationCard
+                  description={this.state.game.information.description}
+                  developers={this.state.game.information.developers}
+                  awards={this.state.game.information.awards}
+                />
+              </Grid.Column>
+              </Grid.Row>
+          </Grid>
+      </Container>
+    );
+  }
 }
