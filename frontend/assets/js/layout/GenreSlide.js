0ef46b5ce82d8098041ec45b5e1ee399599ebff0
ArthurTemporim <arthurrtl@gmail.com>
2017-06-10 19:23:04 -0300

    Create simple genreSlider.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>
    Signed-off-by: leticiadesouza <leticia.souza212@gmail.com>

--- frontend/assets/js/layout/GenreSlide.js
+++ frontend/assets/js/layout/GenreSlide.js
@@ -4,30 +4,74 @@ require("slick-carousel/slick/slick.css");
 require("slick-carousel/slick/slick-theme.css");
 
 export default class GenreSlide extends React.Component {
+  constructor (props) {
+
+      super(props);
+      this.state = {"games": []};
+  }
+
+  componentWillMount () {
+
+      this.loadGameFromServer();
+
+  }
+
+  loadGameFromServer () {
+
+      fetch("/api/list/",
+          {
+              "headers": new Headers({
+                  "Accept": "application/json",
+                  "Content-Type": "application/json",
+              }),
+              "method": "GET"
+          }).
+        then((response) => response.json()).
+        then((games) => {
+
+            this.setState({games});
+
+        });
+
+  }
   render() {
+    const images = this.mountImages();
+
     const settings = {
-      className: 'center',
-      centerMode: true,
+      dots: true,
+      lazyLoad: true,
       infinite: true,
-      centerPadding: '60px',
-      slidesToShow: 3,
-      speed: 500
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      initialSlide: 3
     };
+    if(images.length){
     return (
       <div>
         <h2>Center Mode</h2>
         <Slider {...settings}>
-          <div><h3>1</h3></div>
-          <div><h3>2</h3></div>
-          <div><h3>3</h3></div>
-          <div><h3>4</h3></div>
-          <div><h3>5</h3></div>
-          <div><h3>6</h3></div>
-          <div><h3>7</h3></div>
-          <div><h3>8</h3></div>
-          <div><h3>9</h3></div>
+          {images}
+          {console.log('#######################')}
+          {console.log(this.state.games)}
+          {console.log('#######################')}
         </Slider>
       </div>
     );
+  }else{
+    return <img/>
+  }
+  }
+  mountImages(){
+   const images = [], imagesSlide = 10;
+    for(var idx=0; idx < imagesSlide && idx < this.state.games.length; idx+=1){
+        const image =
+               (<div>
+                        <img src={this.state.games[idx].slide_image} />
+                </div>)
+       images.push(image);
+    
+    }
+    return images;
   }
 }
