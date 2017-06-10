31c7abf9f26515ace96a776c6b74fcc233201e71
Icaro Pires <icaropiresdf@hotmail.com>
2017-06-10 06:53:25 +0000

    Add developers, awards and genres nested relashions with tests
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- backend/information/serializers.py
+++ backend/information/serializers.py
@@ -39,3 +39,24 @@ class InformationSerializer(serializers.ModelSerializer):
                   'genres',
                   'game_id',
                   ]
+
+    def create(self, validated_data):
+        developers = validated_data.pop('developers', [])
+        awards =  validated_data.pop('awards', [])
+        genres =  validated_data.pop('genres', [])
+        information = Information.objects.create(**validated_data)
+
+
+        for developer in developers:
+            new_developer = Developer.objects.get_or_create(**developer)
+            information.developers.add(new_developer[0])
+
+        for award in awards:
+            new_award = Award.objects.get_or_create(**award)
+            information.awards.add(new_award[0])
+
+        for genre in genres:
+            new_genre = Genre.objects.get_or_create(**genre)
+            information.genres.add(new_genre[0])
+
+        return information
--- backend/information/tests/test_serializers.py
+++ backend/information/tests/test_serializers.py
@@ -1,43 +1,64 @@
-from information.models import Award, Developer, Genre
+from information.models import Award, Developer, Genre, Information
 from information.serializers import GenreSerializer
 from information.serializers import DeveloperSerializer
 from information.serializers import AwardSerializer
+from game.factory import GameFactory
+from information.serializers import InformationSerializer
 import pytest
+
+
+@pytest.fixture
+def award_serial():
+    return {
+        "name": 'VGA',
+        "year": 2017,
+        "place": 'UnB'
+    }
+
+
+@pytest.fixture
+def developer_serial():
+    return {
+        'name': 'Developer',
+        'login': 'developer',
+        'github_page': 'http://github.com/developer',
+        'email': 'developer@gmail.com',
+    }
+
+
+@pytest.fixture
+def genre_serial():
+    return {
+        'name': 'Race',
+        'description': 'Games like fast and furious'
+    }
 
 
 class TestGenreSerializer:
 
     @pytest.mark.django_db
-    def test_serialization_genre(self):
+    def test_serialization_genre(self, genre_serial):
         genre = Genre(
             name='Race',
             description='Games like fast and furious')
-
         genre_serialized = GenreSerializer(genre).data
-        expected_json_genre = {
-            'name': 'Race',
-            'description': 'Games like fast and furious'}
-        assert genre_serialized == expected_json_genre
+        assert genre_serialized == genre_serial
 
 
 class TestAwardSerializer:
 
     @pytest.mark.django_db
-    def test_serialization_award(self):
+    def test_serialization_award(self, award_serial):
         award = Award(name='VGA', year='2017', place='UnB')
 
         award_serialized = AwardSerializer(award).data
-        expected_json_award = {
-            "name": 'VGA',
-            "year": 2017,
-            "place": 'UnB'}
-        assert award_serialized == expected_json_award
+        assert award_serial == award_serialized
 
 
 class TestDeveloperSerializer:
 
     @pytest.mark.django_db
-    def test_serialization_developer(self):
+    def test_serialization_developer(self, developer_serial):
         developer = Developer(
             name='Developer',
             avatar='none',
@@ -46,10 +67,53 @@ class TestDeveloperSerializer:
             github_page='http://github.com/developer')
 
         developer_serialized = DeveloperSerializer(developer).data
-        expected_json_developer = {
-            'name': 'Developer',
-            'avatar': '/public/none',
-            'login': 'developer',
-            'email': 'developer@gmail.com',
-            'github_page': 'http://github.com/developer'}
-        assert developer_serialized == expected_json_developer
+        developer_serial['avatar'] = '/public/none'
+        assert developer_serialized == developer_serial
+
+
+class TestInformationSerializer:
+
+    @pytest.fixture
+    def information_serial(self):
+        return {
+            'description': 'a'*51,
+            'launch_year': 1980,
+            'game_id': GameFactory().pk
+        }
+
+    @pytest.mark.django_db
+    def test_nested_developer_save(self, information_serial, developer_serial):
+        developer_serial2 = developer_serial.copy()
+        developer_serial['name'] = 'Developer 2'
+        information_serial['developers'] = [developer_serial2,
+                                            developer_serial]
+
+        serial = InformationSerializer(data=information_serial)
+        assert serial.is_valid()
+        serial.save()
+        assert Information.objects.count() == 1
+        assert Information.objects.last().developers.count() == 2
+
+    @pytest.mark.django_db
+    def test_nested_award_save(self, information_serial, award_serial):
+        award_serial2 = award_serial.copy()
+        award_serial['name'] = 'Award 2'
+        information_serial['awards'] = [award_serial2, award_serial]
+
+        serial = InformationSerializer(data=information_serial)
+        assert serial.is_valid()
+        serial.save()
+        assert Information.objects.count() == 1
+        assert Information.objects.last().awards.count() == 2
+
+    @pytest.mark.django_db
+    def test_nested_genre_save(self, information_serial, genre_serial):
+        genre_serial2 = genre_serial.copy()
+        genre_serial['name'] = 'Genre 2'
+        information_serial['genres'] = [genre_serial2, genre_serial]
+
+        serial = InformationSerializer(data=information_serial)
+        assert serial.is_valid()
+        serial.save()
+        assert Information.objects.count() == 1
+        assert Information.objects.last().genres.count() == 2
