e289a7e2440d2525ef2424b899fc42e50cbab58c
Eduardo Nunes <eduardonunes2525@gmail.com>
2017-06-11 01:33:14 -0300

    Add fields validates for Description and GameInformationCard
    Signed-off-by: Eduardo Nunes <eduardonunes2525@gmail.com>
    Signed-off-by: Varley <varleysilva@me.com>

--- frontend/assets/js/layout/SegmentTitle.js
+++ frontend/assets/js/layout/SegmentTitle.js
@@ -5,7 +5,7 @@ export default class SegmentTitle extends Component {
   render() {
       return (
       	<Segment padded inverted color="grey">
-                <h1>{this.props.title}</h1>
+            <h1>{this.props.title}</h1>
         </Segment>    
       );
     }
