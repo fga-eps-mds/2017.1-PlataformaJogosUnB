bb54c802a9afcfb727d28323a3f61565fe6b44d2
Vinicius <vfbdelima@gmail.com>
2017-06-12 00:46:43 +0000

    Sort by added to games page
    Signed-off-by: Vinicius <vfbdelima@gmail.com>
    Signed-off-by: Rafael Braganca <rabragancaunb@gmail.com>

--- frontend/assets/js/components/GameList.js
+++ frontend/assets/js/components/GameList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {Grid, Container} from "semantic-ui-react";
+import {Grid} from "semantic-ui-react";
 import GameCard from "./cards/GameCard";
 
 export default class GameList extends React.Component {
@@ -8,7 +8,9 @@ export default class GameList extends React.Component {
     constructor (props) {
 
         super(props);
-        this.state = {"games": []};
+        this.state = {
+            "games": [],
+        };
 
     }
 
@@ -42,9 +44,30 @@ export default class GameList extends React.Component {
 
     }
 
+    sortList(){
+         if(typeof this.state.games === "undefined"){
+            return false;
+        }
+        const frontToBack = 'frontToBack', backToFront = 'backToFront';
+        const data = this.props.sortByOption;
+        const listGames = this.state.games;
+        listGames.sort((a,b) => {
+                if(data.order === frontToBack){
+                    if(eval('a'+data.param) > eval('b'+data.param)) return 1;
+                    if(eval('a'+data.param) < eval('b'+data.param)) return -1;
+                } else if(data.order === backToFront){
+                    if(eval('b'+data.param) > eval('a'+data.param)) return 1;
+                    if(eval('b'+data.param) < eval('a'+data.param)) return -1;
+                }
+                return 0;
+            }
+        );
+        return listGames;
+    }
+
     render () {
 
-        const gameCards = this.state.games.map((game) =>
+        const gameCards = this.sortList().map((game) =>
             <Grid.Column mobile={16} tablet={8} computer={4} largeScreen={4}>
                   <Link to={`/games/${game.pk}/${game.name}`}
                         params={{"id": game.pk}}>
@@ -54,11 +77,9 @@ export default class GameList extends React.Component {
           );
 
         return (
-            <Container>
-                <Grid doubling columns={5}>
-                    {gameCards}
-                </Grid>
-            </Container>
+            <Grid doubling columns={5}>
+                {gameCards}
+            </Grid>
         );
 
     }
--- frontend/assets/js/pages/GamesPage.js
+++ frontend/assets/js/pages/GamesPage.js
@@ -1,16 +1,44 @@
 import React from "react";
-import {Container} from "semantic-ui-react";
-import GameList from "../components/GameList";
 import SegmentTitle from "../layout/SegmentTitle";
+import {Container, Grid, Menu} from "semantic-ui-react";
+import GameList from "../components/GameList";
+import SortByItems from "../components/SortByItems";
 
 export default class GamesPage extends React.Component {
+    
+    constructor (){
+        super();
+        this.state = {
+            "sortByOption": 'none'
+        }
+    }
+
+    sortByOptionChanged(option){
+        this.setState({ sortByOption: option });
+    }
 
     render () {
 
         return (
             <Container>
                 <SegmentTitle title={'Lista de Jogos'} />
-                <GameList />
+                <Grid>
+                    <Grid.Row>
+                        <Segment padded inverted color="brown">
+                            <h1>Lista de jogos</h1>
+                        </Segment>
+                    </Grid.Row>
+                    <div>
+                        <Menu inverted>
+                            <Menu.Item>
+                                <SortByItems callbackParent={(option) => this.sortByOptionChanged(option)}/>
+                            </Menu.Item>
+                        </Menu>
+                    </div>
+                    <Grid.Row>
+                        <GameList sortByOption={this.state.sortByOption}/>
+                    </Grid.Row>
+                </Grid>
             </Container>
         );
 
