659d745a4aa811a472e4901e8dfc027c8a16a59d
joaorobson <joaorobsonmartins@gmail.com>
2017-06-12 00:55:35 -0300

    Improve the index slide a little bit more

--- frontend/assets/js/layout/IndexSlide.js
+++ frontend/assets/js/layout/IndexSlide.js
@@ -4,13 +4,11 @@ import React from "react";
 import InfiniteCarousel from "react-leaf-carousel";
 import "style-loader!css-loader!sass-loader!slick-carousel/slick/slick.css"
 import GameCard from "../components/cards/GameCard";
-import {Card} from 'semantic-ui-react';
+import {Card,Icon} from 'semantic-ui-react';
 const imageStyle = {
     "height": 400,
     "margin": "auto",
     "display":"block",
-    "maxWidth":"100%",
-    "minWidth":"100%",
     "width":"100%"
 }, carouselImageStyle = {
     "background": "#000000",
@@ -56,24 +54,24 @@ export default class IndexSlider extends React.Component {
     render () {
         const images = this.mountImages();
 
-var settings = {
-     dots: true,
-      infinite: true,
-fade:true,
-    autoplay: true,
-      autoPlaySpeed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 2,
-        initialSlide:1,
-variableWidth: true
-    };
+        var settings = {
+            dots: true,
+            infinite: true,
+            autoplay:true,
+            autoPlaySpeed: 4200,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide:1
+        };
 
 
-if(images.length){
+        if(images.length){
 
-    return (<div style={imageStyle}>
-      <Slider {...settings}>{images}
-      </Slider></div>
+            return (<div style={imageStyle}>
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+                <Slider {...settings}>{images}
+                </Slider></div>
             );
         }else{
             return <img/>
@@ -85,17 +83,25 @@ if(images.length){
        const images = [], imagesSlide = 9;
 
         for(var idx=0; idx < imagesSlide && idx < this.state.games.length; idx+=1){
+
             var image =
-                   (<div style={{width:789}}>
+                (<div style={{height:400}}>
                             <img
-                               src={this.state.games[idx].slide_image} />
+                               src={this.state.games[idx].slide_image} style={{margin:"0 auto",float:"left",height:"100%",width:"70%"}}/>
+       
+                             <div style={{verticalAlign:"top",float:"right",height:400,width:"30%"}}>
+                                <Card fluid style={{height:400}}>
+                                    <Card.Content>
+                                        <Card.Header>{this.state.games[idx].name}</Card.Header>
+                                            <Card.Description>Esse jogo é muito bolado</Card.Description>   
+                                    </Card.Content> 
+                                        <Card.Content extra>
+                                            <Icon name="linux" />Linux > Windows</Card.Content>
+                                </Card>
+                            </div> 
 
-                        </div>)
-           images.push(image);
-         image =<div style={{width:338}}>    <div ><Card fluid ><Card.Content><Card.Header>Jogo Bolado</Card.Header><Card.Description>Esse jogo é muito bolado</Card.Description></Card.Content></Card> </div>
 
-
-</div>
+                </div>)
 
            images.push(image);
 
