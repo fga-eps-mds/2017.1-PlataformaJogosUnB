6ccd8dfdf715a523dd9464e7d003400cfb40a161
Thiago Pereira <thiagoflames@gmail.com>
2017-06-15 15:08:58 -0300

    Add musician, designe and developer in table
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: Eduardo Nunes <eduardonunes2525@gmail.com>

--- frontend/assets/js/components/cards/DevelopersCard.js
+++ frontend/assets/js/components/cards/DevelopersCard.js
@@ -3,91 +3,57 @@ import {Card, Popup, Table, Header} from "semantic-ui-react";
 import {Link} from "react-router-dom";
 
 export default class DevelopersCard extends React.Component {
-  
-    getDeveloper(credits){
-        var temp = []
-            console.log(credits);
-        if(credits!==undefined){
-            temp = (credits.map((credit) => credit.specialty ))
-            if(temp == "desenvolvedor"){
-                var developer = []
-                developer = (credits.map((credit) => credit.name ))
-                return developer;
-            }
-            else
-                return " ";
-        }
-    };
-
-    getDesign(credits){
-        var temp2 = []
-        if(credits!==undefined){
-            temp2 = (credits.map((credit) => credit.specialty ))
-            if(temp2 == "design"){
-                var design = [] 
-                design = (credits.map((credit) => credit.name ))
-                return design;
-            }
-        else
-            return " ";
-        }
-    };
-
-    getMusician(credits){
-        var temp3 = []
-        if(credits!==undefined){
-            temp3 = (credits.map((credit) => credit.specialty ))
-            if(temp3 == "musico"){
-                var musician = [] 
-                musician = (credits.map((credit) => credit.name ))
-                return musician;
-            }
-        else
-            return " ";
-        }
-    };
-        
+    constructor(props){
+      super(props);
+      this.DEVELOPER = "desenvolvedor";
+      this.DESIGN = "design";
+      this.MUSICIAN = "musico";
+    }
     
+
+    getDeveloper(credit){
+        console.log(credit)
+        if (credit.specialty == "desenvolvedor") {
+          return credit.name;
+        }
+    };
+
+    getRoleCells(role){
+      const credits = this.props.credits;
+      return (<ul>{credits.filter((credit)=> credit.specialty === role)
+                    .map((developer) => <Header.Content>{developer.name}</Header.Content>)}</ul>);
+    }
+
+    getTable(role, title){
+      return (<Table basic='very' celled collapsing>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>
+                    <Header inverted>{title}</Header>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                    <Table.Row>
+                      <Table.Cell key={role}>
+                            {this.getRoleCells(role)}
+                      </Table.Cell>
+                     </Table.Row>
+              </Table.Body>
+            </Table>
+      );
+    }
+
        render () {
+        var desenvolvedor = (this.props.credits).filter(this.getDeveloper);
 
         return (
             <Card fluid>
                 <Card.Content header="Créditos" />
-                <Card.Content extra >
-                    <Table basic='very' celled collapsing>
-                      <Table.Header>
-                        <Table.Row>
-                          <Table.HeaderCell>
-                            <Header inverted>Desenvolvedores</Header>
-                          </Table.HeaderCell>
-                          <Table.HeaderCell>
-                            <Header inverted>Design</Header>
-                          </Table.HeaderCell>
-                           <Table.HeaderCell>
-                            <Header inverted>Musicos</Header>
-                          </Table.HeaderCell>
-                        </Table.Row>
-                      </Table.Header>
-                      <Table.Body>
-                            <Table.Row>
-                              <Table.Cell>
-                                <Header.Content>
-                                    {this.getDeveloper(this.props.credits)}
-                                </Header.Content>
-                              </Table.Cell>
-                              <Table.Cell>
-                                 <Header.Content>
-                                    {this.getDesign(this.props.credits)}
-                                </Header.Content>
-                              </Table.Cell>
-                              <Table.Cell>
-                                 <Header.Content>
-                                    {this.getMusician(this.props.credits)}
-                                </Header.Content>
-                              </Table.Cell>
-                             </Table.Row>
-                      </Table.Body>
-                    </Table>
+                <Card.Content>
+                  {this.getTable(this.DEVELOPER, "Desenvolvedores")}
+                  {this.getTable(this.DESIGN, "Design")}  
+                  {this.getTable(this.MUSICIAN, "Musico")}
                 </Card.Content>
             </Card>
         );
