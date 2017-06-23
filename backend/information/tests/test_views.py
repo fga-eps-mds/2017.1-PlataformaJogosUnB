8836a1424141b8556a412ae152ec7f4524fd0590
Artur Bersan de Faria <artur_bersan@hotmail.com>
2017-06-23 13:45:28 +0000

    Post test view

--- backend/information/tests/test_views.py
+++ backend/information/tests/test_views.py
@@ -7,6 +7,22 @@ from information.serializers import GenreSerializer
 class TestGenreList:
 
     @pytest.fixture
+    def response_list(self, client):
+        return client.get('/api/genres/')
+
+    @pytest.mark.django_db
+    def test_access_route_genres(self, response_list):
+        assert 200 <= response_list.status_code < 300
+
+    @pytest.mark.django_db
+    def test_data_genres(self, response_list):
+        genres = Genre.objects.all()
+        assert response_list.data is not None
+        assert response_list.data == GenreSerializer(genres, many=True).data
+
+class TestVoteView:
+
+    @pytest.fixture
     def information(self):
         information = InformationFactory()
         rating = Rating(vote=True,email_voter="test@test.com",information=information)
@@ -14,12 +30,10 @@ class TestGenreList:
         return information
 
     @pytest.fixture
-    def response_list(self, client):
-        return client.get('/api/genres/')
-
-    @pytest.mark.django_db
-    def test_access_route_genres(self, response_list):
-        assert 200 <= response_list.status_code < 300
+    def rating(self, information):
+        rating = Rating(vote=True,email_voter="test@test2.com",information=information)
+        rating.save()
+        return rating
 
     @pytest.fixture
     def response_vote(self, client, information):
@@ -31,13 +45,18 @@ class TestGenreList:
         assert 200 <= response_vote.status_code < 300
 
     @pytest.mark.django_db
-    def test_data_genres(self, response_list):
-        genres = Genre.objects.all()
-        assert response_list.data is not None
-        assert response_list.data == GenreSerializer(genres, many=True).data
-
-    @pytest.mark.django_db
     def test_data_vote(self, response_vote):
         vote = {'likes': 1,'dislikes': 0}
         assert response_vote is not None
         assert response_vote.data == vote
+
+    @pytest.mark.django_db
+    def test_post(self, client, information, rating):
+        response = client.post("/api/vote/{0}".format(information.pk),
+                               data=rating,
+                               content_type="application/json")
+        print(response)
+        print(information.likes)
+        assert 200 <= response.status_code < 300
+        assert information.likes == 2
+   
