cfc355fe1566627a0f76d6c9f3e800c32ba5020e
joaorobson <joaorobsonmartins@gmail.com>
2017-06-24 20:13:39 +0000

    Icons filter functions

---
+++ frontend/assets/js/resources/getKernel.js
@@ -0,0 +1,18 @@
+
+export const getKernel = function( packages) {
+    const extensionsByKernel = {
+        linux: ["deb","rpm"],
+        windows: ["exe"],
+        apple: ["app"]
+    };
+    const kernels = [];
+    packages.map((item) => {
+       item.platforms.map((extension) => {
+        Object.getOwnPropertyNames(extensionsByKernel).forEach(function(value){
+                if(extensionsByKernel[value].indexOf(extension.extensions) > -1){
+                    kernels.push(value);
+                }       
+            })      
+       })})   
+return kernels 
+} 
