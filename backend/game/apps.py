11c911725da0d56a27d55633ec983fd3513953b5
Marcelo Ferreira <marcelohpf@gmail.com>
2017-08-27 00:26:03 -0300

    Add configuration to translate app name
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- backend/game/apps.py
+++ backend/game/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class GameConfig(AppConfig):
     name = 'game'
+    verbose_name = _('game')
     # label = 'my.core'
--- backend/media/apps.py
+++ backend/media/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class MediaConfig(AppConfig):
     name = 'media'
+    verbose_name = _('media')
